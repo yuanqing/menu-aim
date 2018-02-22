89261d1a34e81aaac8c7367f30e3c123e6a40bdd
yuanqing <desk@yuanqing.sg>
2018-02-22 23:06:47 +0800

    Remove redundant functions

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Compute the top and left coordinate of the given `element`.
 function calculateExtremeCoordinates (element) {
   var rect = element.getBoundingClientRect()
   var topX =
@@ -15,38 +14,21 @@ function computeGradient (pointA, pointB) {
   return (pointB.y - pointA.y) / (pointB.x - pointA.x)
 }
 
-function createClassManipulationFunction (classListMethod) {
-  return function (element, classNames) {
-    classNames.forEach(function (className) {
-      element.classList[classListMethod](className)
-    })
-  }
-}
-var addClasses = createClassManipulationFunction('add')
-var removeClasses = createClassManipulationFunction('remove')
-
 module.exports = function (menuElement, options) {
-  var menuDirection = options.menuDirection || 'right'
   var delay = options.delay || 200
   var menuItemSelector = options.menuItemSelector || '.menu-aim__item'
   var menuItemSubMenuSelector =
     options.menuItemSubMenuSelector || '.menu-aim__item-submenu'
-  var menuItemActiveClassName = [].concat(
+  var menuItemActiveClassName =
     options.menuItemActiveClassName || 'menu-aim__item--active'
-  )
-  var delayingClassName = [].concat(
-    options.delayingClassName || 'menu-aim--delaying'
-  )
+  var delayingClassName = options.delayingClassName || 'menu-aim--delaying'
 
   var timeoutId
-
   var activeMenuItem
   var activeSubMenu
   var activeSubMenuOffset
-
   var increasingCorner
   var decreasingCorner
-
   var previousCoordinates = {}
   var currentCoordinates = {}
 
@@ -100,8 +82,8 @@ module.exports = function (menuElement, options) {
 
   function deactivateActiveMenuItem () {
     if (activeMenuItem) {
-      removeClasses(menuElement, delayingClassName)
-      removeClasses(activeMenuItem, menuItemActiveClassName)
+      menuElement.classList.remove(delayingClassName)
+      activeMenuItem.classList.remove(menuItemActiveClassName)
       activeMenuItem = null
       increasingCorner = null
       decreasingCorner = null
@@ -110,7 +92,7 @@ module.exports = function (menuElement, options) {
 
   function activateMenuItem (menuItem) {
     activeMenuItem = menuItem
-    addClasses(activeMenuItem, menuItemActiveClassName)
+    activeMenuItem.classList.add(menuItemActiveClassName)
     activeSubMenu = activeMenuItem.querySelector(menuItemSubMenuSelector)
     activeSubMenuOffset = calculateExtremeCoordinates(activeSubMenu)
     increasingCorner = {
@@ -134,36 +116,34 @@ module.exports = function (menuElement, options) {
       activateMenuItem(menuItem)
       return
     }
-    addClasses(menuElement, delayingClassName)
+    menuElement.classList.add(delayingClassName)
     timeoutId = setTimeout(function () {
       possiblyActivateMenuItem(menuItem)
     }, delay)
   }
 
   function onWindowMouseMove (event) {
-    var x = event.pageX
-    var y = event.pageY
     previousCoordinates.x = currentCoordinates.x
     previousCoordinates.y = currentCoordinates.y
-    currentCoordinates.x = x
-    currentCoordinates.y = y
+    currentCoordinates.x = event.pageX
+    currentCoordinates.y = event.pageY
     if (activeMenuItem) {
       possiblyDeactivateActiveMenuItem()
     }
   }
 
-  var menuItems = Array.prototype.slice.call(
-    menuElement.querySelectorAll(menuItemSelector)
-  )
-  menuItems.forEach(function (menuItem) {
-    menuItem.addEventListener('mouseenter', onMenuItemMouseEnter)
-  })
+  var menuItems = menuElement.querySelectorAll(menuItemSelector)
+  var i = menuItems.length
+  while (i--) {
+    menuItems[i].addEventListener('mouseenter', onMenuItemMouseEnter)
+  }
   window.addEventListener('mousemove', onWindowMouseMove)
 
   return function () {
-    menuItems.forEach(function (menuItem) {
-      menuItem.removeEventListener('mouseenter', onMenuItemMouseEnter)
-    })
+    i = menuItems.length
+    while (i--) {
+      menuItems[i].removeEventListener('mouseenter', onMenuItemMouseEnter)
+    }
     window.removeEventListener('mousemove', onWindowMouseMove)
   }
 }
